2e766d3662ff36fb63bf3f16a78599c8a65e332e
Terry Tong <terry.lk.tong@gmail.com>
2014-11-26 21:46:49 +0800

    Finished Chapter 9; added Cart & Line Items.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,17 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true,
             format: { with: %r{\.(gif|jpg|png)\Z}i, message: "must be a URL for GIF, JPG, or PNG image." }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  # Ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
--- db/migrate/20141119095253_create_products.rb
+++ db/migrate/20141119095253_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :title
-      t.text :description
-      t.text :image_url
+      t.string  :title
+      t.text    :description
+      t.text    :image_url
       t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
